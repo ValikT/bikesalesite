48f009b263836aa61ae57a347c9f8049b54e8a27
ValikT <valentint711@gmail.com>
2017-12-22 18:30:33 +0200

    models fix in admin menu

--- app/models/model.rb
+++ app/models/model.rb
@@ -3,11 +3,7 @@ class Model < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
-  has_many :posts    
-  has_many :photos
-  has_many :postcomments  
-  has_many :votes 
+   
 
   validates :nickname, presence: true 
   validates :nickname, length: { in: 3..12 }
